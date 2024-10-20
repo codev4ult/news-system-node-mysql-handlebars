e5a0979e096fcaf8157cbf7941e98549619ee4e8
Rogério Soares <rgrsoares@yahoo.com.br>
2024-10-20 19:41:33 -0300

    Fix bug upload images

--- src/routes/newsRoutes.js
+++ src/routes/newsRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import multer from "multer";
+import path, { format } from "path";
 import Author from "../models/authorSchema.js";
 import Category from "../models/categorySchema.js";
 import News from "../models/newsSchema.js";
@@ -7,18 +8,27 @@ import News from "../models/newsSchema.js";
 const router = express.Router();
 
 // Configuração do multer para upload de imagens
-const upload = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/assets/img/");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage });
 
 router.get("/register", async (req, res) => {
     try {
         const authors = await Author.find().lean();
         const categories = await Category.find().lean();
-        res.render("newsForm", { authors, categories, title : "Inserting news" }); // Renderiza o formulário de criação de notícias
+        res.render("newsForm", { authors, categories, title: "Inserting news" }); // Renderiza o formulário de criação de notícias
     } catch (error) {
         console.log(error);
         res.render("newsForm", {
             message: { type: "danger", text: "Error fetching authors or categories" },
-            title : "Insert news"
+            title: "Insert news",
         });
     }
 });
@@ -27,27 +37,39 @@ router.get("/register", async (req, res) => {
 router.post("/insert", upload.single("image"), async (req, res) => {
     try {
         const { title, content, category, author, status } = req.body;
-        const image = req.file ? req.file.filename : ""; // Obtém o nome do arquivo da imagem
 
-        const news = new News({
-            title,
-            content,
-            category,
-            author,
-            image,
-            status,
-        });
+        const image = req.file ? req.file.originalname : "";
 
-        await news.save();
-        res.render("newsForm", {
-            message: { type: "success", text: "News created successfully!" },
-            title : "Insert news"
-        });
+        // Verifica se já existe uma notícia com o mesmo título
+        const existingNews = await News.findOne({ title }).lean();
+
+        if (!existingNews) {
+            const news = new News({
+                title,
+                content,
+                category,
+                author,
+                image,
+                status,
+            });
+
+            await news.save();
+
+            res.render("newsForm", {
+                message: { type: "success", text: "News created successfully!" },
+                title: "Insert news",
+            });
+        } else {
+            res.render("newsForm", {
+                message: { type: "danger", text: "Notícia já está cadastrada" },
+                title: "Error: Insert news",
+            });
+        }
     } catch (error) {
         console.log(error);
         res.render("newsForm", {
-            message: { type: "danger", text: "Error creating news post" },
-            title : "Insert news"
+            message: { type: "danger", text: `Error creating news post: ${error.message}` },
+            title: "Insert news",
         });
     }
 });
